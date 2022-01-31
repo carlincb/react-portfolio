4f4015fe511a872001e706613f525a11ca14a02b
carlincb <carlin.colleen@gmail.com>
2022-01-30 20:04:31 -0700

    Finished App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, Portfolio } from './pages';
 
 function App() {
   return (
     <div className="App">
       < Router >
-      
+      < Route exact path='/' component={ Home }/>
+      < Route exact path='/Portfolio' component={ Portfolio }/>
       </Router>
     </div>
   );
